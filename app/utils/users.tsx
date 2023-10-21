4b3df2e5c649342e72807325ed67a6c1b83ae71a
Austinpjh <66819152+Austinpjh@users.noreply.github.com>
2023-10-21 23:57:59 +0800

    Update users.tsx

--- app/utils/users.tsx
+++ app/utils/users.tsx
@@ -1,24 +1,35 @@
 import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
-export const createUser = (email: any, username: any, displayName: any) => {
-  return firestore()
+export const createUser = (
+  email: any,
+  username: any,
+  displayName: any,
+  uid: any,
+) => {
+  firestore()
     .collection('Users')
-    .add({
+    .doc(uid)
+    .set({
       displayName: displayName,
       username: username,
       email: email,
       exp: 0,
-      friends: [{username: '', exp: 0}],
-      hearts: 5,
+      hearts: {
+        amount: 5,
+        timestamp: 0,
+      },
       modules: {
         easy: 9,
         intermediate: 9,
         hard: 9,
       },
-    })
-    .then(() => {
-      console.log('User created.');
+      avatar: 0,
     });
+  console.log('User created.');
+
+  // Cannot create empty collection, a document must be added
+  firestore().collection('Users').doc(uid).collection('Friends').add({});
 };
 
 export const getUserID = () => {
@@ -38,18 +49,6 @@ export const UpdateUsername = (userAccount: any, username: any) => {
     .doc(userAccount)
     .update({
       name: username,
-    })
-    .then(() => {
-      console.log('User updated!');
-    });
-};
-
-export const AddFriend = (userAccount: any, username: any, exp: any) => {
-  return firestore()
-    .collection('Users')
-    .doc(userAccount)
-    .update({
-      friends: [{name: username, exp: exp}],
     })
     .then(() => {
       console.log('User updated!');
@@ -105,4 +104,50 @@ export const DeleteAccount = (userAccount: any) => {
     .then(() => {
       console.log('User deleted!');
     });
+};
+
+export const createFriend = (username: any) => {
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      const uid = user.uid;
+      firestore()
+        .collection('Users')
+        .where('username', '==', username)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(documentSnapshot => {
+            console.log(documentSnapshot.id);
+            firestore()
+              .collection('Users')
+              .doc(uid)
+              .collection('Friends')
+              .doc(documentSnapshot.id)
+              .set({});
+          });
+        });
+    }
+  });
+};
+
+export const getFriendList: any = () => {
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      const uid = user.uid;
+      var friendList: string[] = [];
+      firestore()
+        .collection('Users')
+        .doc(uid)
+        .collection('Friends')
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(documentSnapshot => {
+            friendList = [...friendList, documentSnapshot.id];
+          });
+          // remove first document added during account creation ()
+          friendList.shift();
+          console.log(friendList);
+          return friendList;
+        });
+    }
+  });
 };
