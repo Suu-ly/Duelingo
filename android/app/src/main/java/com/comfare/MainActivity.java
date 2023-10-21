dc1507e09c2be1380ee38ccbd41b8bd59717ab3f
mercykillsjoy <victor.choy23@gmail.com>
2023-10-22 00:38:19 +0800

    splash screen

--- android/app/src/main/java/com/comfare/MainActivity.java
+++ android/app/src/main/java/com/comfare/MainActivity.java
@@ -5,7 +5,20 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import android.os.Bundle; // here
+
+import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
+// react-native-splash-screen < 0.3.1 
+
 public class MainActivity extends ReactActivity {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+        /SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is
