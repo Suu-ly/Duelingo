a9ae33b5d8670bc2585906b9258779eb820e70e8
weiss-cha <scamhak123@gmail.com>
2023-10-21 23:49:58 +0800

    Add friend & Get friend list

--- app/utils/database.tsx
+++ app/utils/database.tsx
@@ -1,4 +1,5 @@
 import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 // Get All Quizzes
 export const getAllQuiz = () => {
@@ -37,5 +38,52 @@ export const createUser = (
     });
   console.log('User created.');
 
+  // Cannot create empty collection, a document must be added
   firestore().collection('Users').doc(uid).collection('Friends').add({});
+};
+
+export const createFriend = (username: any) => {
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      const uid = user.uid;
+      firestore()
+        .collection('Users')
+        .where('username', '==', username)
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(documentSnapshot => {
+            console.log(documentSnapshot.id);
+            firestore()
+              .collection('Users')
+              .doc(uid)
+              .collection('Friends')
+              .doc(documentSnapshot.id)
+              .set({});
+          });
+        });
+    }
+  });
+};
+
+export const getFriendList: any = () => {
+  auth().onAuthStateChanged(user => {
+    if (user) {
+      const uid = user.uid;
+      var friendList: string[] = [];
+      firestore()
+        .collection('Users')
+        .doc(uid)
+        .collection('Friends')
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(documentSnapshot => {
+            friendList = [...friendList, documentSnapshot.id];
+          });
+          // remove first document added during account creation ()
+          friendList.shift();
+          console.log(friendList);
+          return friendList;
+        });
+    }
+  });
 };
