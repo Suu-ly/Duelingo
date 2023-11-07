de29cecb88866b09ba9ef10e37cd6603defd00bb
hikannang <noelleonggg@gmail.com>
2023-11-07 15:56:00 +0800

    update to impactHeavy

--- app/common/TopicButton.tsx
+++ app/common/TopicButton.tsx
@@ -37,7 +37,7 @@ const TopicButton = (props: ButtonProps) => {
   } = props;
 
   const handlePress =() => {
-    ReactNativeHapticFeedback.trigger('impactLight', {
+    ReactNativeHapticFeedback.trigger('impactHeavy', {
       enableVibrateFallback: true,
       ignoreAndroidSystemSettings: false,
     });
